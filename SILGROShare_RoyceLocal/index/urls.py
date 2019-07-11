b21f06f020f4a1df0a22332ec9bf590baa4165c1
xiaoyuyu224 <zhi2500@126.com>
2019-07-11 13:04:14 +0800

    Add homePage url

--- SILGROShare_RoyceLocal/index/urls.py
+++ SILGROShare_RoyceLocal/index/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.homePage),
     #添加带有字符类型，整形，和slug的类型变量的URL
     path('<year>/<int:month>/<slug:day>',views.mydate),#变量名为year,month,day
     #这里也可以用正则表达式来限制变量的范围
